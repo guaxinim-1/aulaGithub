1dfb0d5e315bd66633d554dc0f39ab79ab2bc3c2
joao vitor <joao550vit@gmail.com>
2024-09-23 16:50:32 -0400

    caiu xampu no oio2

--- py.py
+++ py.py
@@ -1,8 +1,10 @@
 print("caiu xampu no oio")
 
-tirarxampu=input("voce quer tirar o xampu do oio---sim/nao:")
+tirarxampu=input("voce quer tirar o xampu do oio---sim/nao/kkkkxampu:")
 
 if tirarxampu=="sim":
     print("vc tirou o xampu do oio")
+elif tirarxampu=="kkkkxampu":
+    print("kkkkxampu haaaaaa")
 else:
     print("ta ardentu")

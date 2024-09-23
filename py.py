cdf127489b8d4a316c3e1c2100ab1fd131f4f028
joao vitor <joao550vit@gmail.com>
2024-09-23 16:39:14 -0400

    caiu xampu no oio

---
+++ py.py
@@ -0,0 +1,8 @@
+print("caiu xampu no oio")
+
+tirarxampu=input("voce quer tirar o xampu do oio---sim/nao:")
+
+if tirarxampu=="sim":
+    print("vc tirou o xampu do oio")
+else:
+    print("ta ardentu")
